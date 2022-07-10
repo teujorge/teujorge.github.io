7460e42a2bc7c732b3b6d277a3a7787caba82a3c
Matheus <mrljorge@outlook.com>
2022-07-10 13:20:26 -0300

    using interactive viewer

--- lib/resume.dart
+++ lib/resume.dart
@@ -16,13 +16,28 @@ class _ResumePageState extends State<ResumePage> {
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
     _body = Center(
       child: Padding(
         padding: const EdgeInsets.all(24),
         child: ClipRRect(
           borderRadius: const BorderRadius.all(Radius.circular(24)),
-          child: SingleChildScrollView(
-            child: Image.asset("Resume-2022.png"),
+          child: Container(
+            color: Colors.black,
+            child: InteractiveViewer(
+              alignPanAxis: true,
+              constrained: false,
+              minScale: 0.1,
+              maxScale: 4,
+              child: Image.asset(
+                "Resume-2022.png",
+                width: width * 0.8,
+                height: height * 0.75,
+                fit: BoxFit.contain,
+              ),
+            ),
           ),
         ),
       ),
