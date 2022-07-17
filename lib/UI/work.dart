9835867b31f84c15e6356f960db3a5997e5365e2
Matheus <mrljorge@outlook.com>
2022-07-17 18:07:16 -0300

    overhaul (layout + colors)

--- lib/UI/work.dart
+++ lib/UI/work.dart
@@ -13,114 +13,74 @@ class Work extends StatefulWidget {
 class _WorkState extends State<Work> {
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
-      constraints: BoxConstraints.tightFor(
-        width: Config.size.width,
-        // height: Config.size.height * 1.2,
-      ),
+    return SizedBox(
+      width: Config.size.width,
       child: Column(
         children: [
           MainHeader(number: "02.", text: "Experience"),
           SizedBox(
             height: Config.size.height * 0.07,
           ),
-          Row(
-            children: [
-              Expanded(
-                flex: 1,
-                child: Container(
-                  height: Config.size.height,
-                  child: Stack(
-                    children: [
-                      Center(
-                        child: VerticalDivider(
-                          thickness: 1.75,
-                          width: 10,
-                          indent: 10,
-                          endIndent: 10,
-                        ),
+          Container(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  WorkData(
+                    title:
+                        "Test Software Engineer, Honeywell Aerospace, Clearwater FL",
+                    subTitle:
+                        "Translated written requirements into software. Perform software validation and documentation. Communicate and consult with clients to ameliorate current manufacturing processes. Support product manufacturing by maintaining and updating factory equipment.",
+                    duration: "2020 - 2022",
+                    avatar: CircleAvatar(
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      child: FaIcon(
+                        // FontAwesomeIcons.laptopCode,
+                        FontAwesomeIcons.laptop,
                       ),
-                      Container(
-                        child: Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              CircleAvatar(
-                                backgroundColor:
-                                    Theme.of(context).colorScheme.primary,
-                                child: FaIcon(
-                                  // FontAwesomeIcons.laptopCode,
-                                  FontAwesomeIcons.laptop,
-                                ),
-                              ),
-                              CircleAvatar(
-                                backgroundColor:
-                                    Theme.of(context).colorScheme.secondary,
-                                child: FaIcon(
-                                  FontAwesomeIcons.codeBranch,
-                                ),
-                              ),
-                              CircleAvatar(
-                                backgroundColor:
-                                    Theme.of(context).colorScheme.primary,
-                                child: FaIcon(
-                                  FontAwesomeIcons.freeCodeCamp,
-                                ),
-                              ),
-                              CircleAvatar(
-                                backgroundColor:
-                                    Theme.of(context).colorScheme.secondary,
-                                child: FaIcon(
-                                  FontAwesomeIcons.mugHot,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Expanded(
-                  flex: Config.smallScreen ? 5 : 4,
-                  child: Container(
-                    // height: Config.size.height,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        WorkData(
-                          title:
-                              "Test Software Engineer, Honeywell Aerospace, Clearwater FL",
-                          subTitle:
-                              "Translate written requirments into software. Perform software validation and documentation. Communicate and consult with clients to ameliorate current manufacturing processes. Support product manufacturing by maintaining and updating factory equipment.",
-                          duration: "2020 - 2022",
-                        ),
-                        WorkData(
-                          title:
-                              "Systems Engineer Inter, BendixKing, Albuquerque NM",
-                          subTitle:
-                              "helped with faa product certification.  wrote and completed integration test plans for iterative software builds. created autonomous processes to repeatdly test systems to ensure product requirements wer satisfied.",
-                          duration: "Summer 2019",
-                        ),
-                        WorkData(
-                          title:
-                              "Technician, University of Vermont, Burlington VT",
-                          subTitle:
-                              "in the university fabrication laboratory I assited with variouse student projects using 3D printers, 3D scanners, laser cutters or soldering equipment. Maintina and update equipment used in lab for efficacy.",
-                          duration: "2018-2019",
-                        ),
-                        WorkData(
-                          title:
-                              " BSe Mechanical Engineering with Computer Science Minor",
-                          subTitle: "",
-                          duration: "May 2020",
-                        ),
-                      ],
                     ),
-                  ))
-            ],
+                  ),
+                  WorkData(
+                    title:
+                        "Systems Engineer Intern, BendixKing, Albuquerque NM",
+                    subTitle:
+                        "Helped with FAA product certification. Wrote and completed integration test plans for iterative software builds. Created autonomous processes to repeatedly test systems in order to ensure product requirements were satisfied.",
+                    duration: "2019",
+                    avatar: CircleAvatar(
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      child: FaIcon(
+                        FontAwesomeIcons.codeBranch,
+                      ),
+                    ),
+                  ),
+                  WorkData(
+                    title:
+                        "Mechanical Engineering Major with Computer Science Minor",
+                    subTitle:
+                        "Bachelors of Science from the University of Vermont.",
+                    duration: "2020",
+                    avatar: CircleAvatar(
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      child: FaIcon(
+                        FontAwesomeIcons.mugHot,
+                      ),
+                    ),
+                  ),
+                  WorkData(
+                    title: "Technician, University of Vermont, Burlington VT",
+                    subTitle:
+                        "In the University Fabrication Laboratory (FabLab), I assisted various student projects that used 3D Printers, 3D Scanners, Laser Cutter or Soldering equipment. Maintained and updated equipment used in lab to ensure efficacy.",
+                    duration: "2018 - 2019",
+                    avatar: CircleAvatar(
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      child: FaIcon(
+                        FontAwesomeIcons.freeCodeCamp,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           )
         ],
       ),
--- lib/Widget/work_data.dart
+++ lib/Widget/work_data.dart
@@ -6,46 +6,88 @@ class WorkData extends StatelessWidget {
   final String title;
   final String subTitle;
   final String duration;
+  final CircleAvatar avatar;
 
-  const WorkData(
-      {super.key,
-      required this.title,
-      required this.subTitle,
-      required this.duration});
+  const WorkData({
+    super.key,
+    required this.title,
+    required this.avatar,
+    required this.subTitle,
+    required this.duration,
+  });
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: <Widget>[
-        Text(
-          title,
-          style: TextStyle(
-            fontSize: Config.smallScreen ? 10 + Config.size.width / 50 : 22.0,
-            fontWeight: FontWeight.w700,
-          ),
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 20, horizontal: 4),
+      child: IntrinsicHeight(
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Stack(
+                children: [
+                  // Center(
+                  //   child:
+                  VerticalDivider(
+                    thickness: 1.75,
+                    width: 10,
+                    indent: 10,
+                    endIndent: 10,
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+                  // ),
+                  Center(child: avatar),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: Config.smallScreen ? 4 : 10,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  // title
+                  Text(
+                    title,
+                    style: TextStyle(
+                      fontSize: Config.smallScreen
+                          ? 10 + Config.size.width / 50
+                          : 20 + Config.size.width / 150,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 6.0,
+                  ),
+                  // sub title
+                  Text(
+                    subTitle,
+                    style: TextStyle(
+                      fontSize: Config.smallScreen
+                          ? 8 + Config.size.width / 75
+                          : 10 + Config.size.width / 225,
+                      fontWeight: Config.smallScreen ? null : FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 6.0,
+                  ),
+                  // date
+                  Text(
+                    duration,
+                    style: TextStyle(
+                      fontSize: Config.smallScreen
+                          ? 10 + Config.size.width / 75
+                          : 12 + Config.size.width / 225,
+                      fontWeight: Config.smallScreen ? null : FontWeight.w700,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-        SizedBox(
-          height: 6.0,
-        ),
-        Text(
-          subTitle,
-          style: TextStyle(
-            fontSize: 13.0,
-            fontWeight: Config.smallScreen ? null : FontWeight.w600,
-          ),
-        ),
-        SizedBox(
-          height: 6.0,
-        ),
-        Text(
-          duration,
-          style: TextStyle(
-            fontSize: 12.0,
-            fontWeight: Config.smallScreen ? null : FontWeight.w700,
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
